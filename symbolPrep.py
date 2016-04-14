260a4923d12880931c177baeff704aaba779e1e0
Michelle <mjsalv@gmail.com>
2016-04-14 16:47:41 -0600

    changes to symbolPrep file

--- symbolPrep.py
+++ symbolPrep.py
@@ -1,31 +1,19 @@
 #!/usr/bin/env python
-import sys
-import wave
-import math
-import struct
 import numpy as np
 import random
-import argparse
-from itertools import izip_longest
 
+def symbolConverter(input)
 
-
-
-def grouper(iterable, n, fillvalue=None):
-    "Collect data into fixed-length chunks or blocks"
-    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
-    args = [iter(iterable)] * n
-    return izip_longest(fillvalue=fillvalue, *args)
-
-N = 11
-
-data_in = np.random.random_integers(0,1,N)
-print 'data in', data_in
-
-#put into 5 bit chunks and convert to decimal for symbols..
-for chunk in grouper(data_in,5,''):
-	for index in range(4)
-	symbol += (int(chunk[index])*2^4-index)
-	print symbol
+	symbolData =[]
+	#put into 5 bit chunks and return decimal for symbols..
+	symbol = 0
+	for i in xrange(0, len(input), 5):
+		chunk = data_in[i:i+5]
+		print chunk
+		for b in range(len(chunk)):
+			symbol += chunk[b]*(2**(len(chunk)-b-1))
+		print 'symbol..............', symbol
+		symbol = 0
+		symbolData.append(symbol)
 
 
